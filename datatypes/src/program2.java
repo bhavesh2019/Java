4030f3e000bcf934159ce964e19e4afe413fa852
bhavesh2019 <bhaveshkumar889@gmail.com>
2019-01-18 14:12:46 +0530

    update

---
+++ datatypes/src/program2.java
@@ -0,0 +1,8 @@
+class program2{
+	public static void main(String args[]){
+		char c='c';
+		int i='c';
+		System.out.println("Integer representation of entered character is");
+		System.out.println(i);
+	}
+}
